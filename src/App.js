62143d67ae84967866c48d26da617e66ddac6804
Abhishek Das <abhishekdaszy@gmail.com>
2025-07-20 23:46:25 +0530

    updated json

--- src/App.js
+++ src/App.js
@@ -8,9 +8,9 @@ const App = () => {
   return (
     <div>
       {/* <ToolDetails/> */}
-      {/* <FormInput/> */}
+      <FormInput/>
       {/* <EditableDetails/> */}
-      <Prac/>
+      {/* <Prac/> */}
     </div>
   )
 }
